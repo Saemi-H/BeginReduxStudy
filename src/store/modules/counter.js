cb3b6c67e27e47aacbed8a9122c5598da188ba3c
Saemi_hong <sstyle940104@gmail.com>
2022-01-04 09:43:10 +0900

    initial commit

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,1 +1,31 @@
 // 카운터 관련 상태 로직
+import { createAction } from "redux-actions";
+
+
+// define action tyle
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+// actions 
+// export to use in different files
+export const increment=()=>({type: INCREMENT});
+export const decrement=()=>({type: DECREMENT});
+
+// define initial state
+const initialState={
+    number : 0
+};
+
+// create Reducer, export
+export default function reducer(state=initialState, action){
+    // return depends on action
+    // define state = initialState
+    switch(action.type){
+        case INCREMENT:
+            return {number : state.number + 1};
+        case DECREMENT:
+            return {number : state.number - 1};
+        default:
+            return state;
+    }
+}
