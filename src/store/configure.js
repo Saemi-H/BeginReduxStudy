2cb376c7ff4eafe5b9e9c73409c91ce01ed26e39
Saemi_hong <sstyle940104@gmail.com>
2022-01-04 10:29:32 +0900

    feat : Change createStore in configure

--- src/store/configure.js
+++ src/store/configure.js
@@ -5,7 +5,11 @@ import { createStore } from "redux";
 import modules from "./modules";
 
 const configure =()=>{
-    const store = createStore(modules);
+    //const store = createStore(modules);
+
+    // to use Redux devtools
+    const devTools = window.__REDUX_DEVTOOLS_EXTENSION__&&window.__REDUX_DEVTOOLS_EXTENSION__()
+    const store = createStore(modules, devTools);
     return store;
 }
 
