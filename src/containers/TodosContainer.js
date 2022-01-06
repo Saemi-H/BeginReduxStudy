fa327a905b2b5d4599e523afa4f744c25654cb43
Saemi_hong <sstyle940104@gmail.com>
2022-01-06 11:27:12 +0900

    fix : fix TodoContianers

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -3,32 +3,35 @@ import React, { Component } from 'react';
 import Todos from 'components/Todos';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as todosActions from 'store/modules/todo';
+//import * as todosActions from 'store/modules/todo';
+//import * as TodoActions from 'store/actionCreators'
+import {TodoActions} from 'store/actionCreators'
 
 class TodosContainer extends Component {
   handleChange = (e) => {
     // 인풋 값 변경
-    const { TodosActions } = this.props;
-    TodosActions.changeInput(e.target.value);
+    //const { TodosActions } = this.props;
+    TodoActions.changeInput(e.target.value);
   }
 
   handleInsert = () => {
     // 아이템 추가
-    const { input, TodosActions } = this.props;
-    TodosActions.insert(input); // 추가하고
-    TodosActions.changeInput(''); // 기존 인풋값 비우기
+    // const { input, TodosActions } = this.props;
+    const { input } = this.props;
+    TodoActions.insert(input); // 추가하고
+    TodoActions.changeInput(''); // 기존 인풋값 비우기
   }
 
   handleToggle = (id) => {
     // 삭제선 켜고 끄기
-    const { TodosActions } = this.props;
-    TodosActions.toggle(id);
+    //const { TodosActions } = this.props;
+    TodoActions.toggle(id);
   }
 
   handleRemove = (id) => {
     // 아이템 제거
-    const { TodosActions } = this.props;
-    TodosActions.remove(id);
+    //const { TodosActions } = this.props;
+    TodoActions.remove(id);
   }
 
   render() {
@@ -52,10 +55,10 @@ export default connect(
   // state 를 비구조화 할당 해주었습니다
   ({ todo }) => ({
     // immutable 을 사용하니, 값을 조회 할 때엔느 .get 을 사용해주어야하죠.
-    input: todo.get('input'),
-    todos: todo.get('todos')
-  }),
-  (dispatch) => ({
-    TodosActions: bindActionCreators(todosActions, dispatch)
+    input: todo.input,
+    todos: todo.todos
   })
+//   (dispatch) => ({
+//     TodosActions: bindActionCreators(todosActions, dispatch)
+//   })
 )(TodosContainer);
